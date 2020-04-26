3c5797e0354f506fd7e8545b0f52cb9cd82ce39c
Richard Cheney <richeney@microsoft.com>
2020-04-26 17:22:39 +0100

    End of the labs

--- controllers/front.go
+++ controllers/front.go
@@ -1,12 +1,20 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 // RegisterControllers : Register a new user controller if passed /users
 func RegisterControllers() {
-
 	uc := newUserController()
 
 	http.Handle("/users", *uc)
 	http.Handle("/users/", *uc)
+}
+
+func encodeResponseAsJSON(data interface{}, w io.Writer) {
+	enc := json.NewEncoder(w)
+	enc.Encode(data)
 }
